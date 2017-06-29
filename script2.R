3af3707ec8e0e668c1c771748101cab81f4a0ee3
damladokuzoglu <damla.dokuzoglu@gauss.com.tr>
2017-06-29 15:25:27 +0300

    message

--- script2.R
+++ script2.R
@@ -1,2 +1,22 @@
 help
 help()
+fhbkjvnfşkbv
+
+
+library(RODBC)
+dsn.name <- "PROD"
+user.name <- "odemtpe"
+
+con1 <- odbcConnect(dsn=dsn.name,uid=user.name,"bucak095")
+
+table.list <- sqlTables(con1,tableType="TABLE", schema="ODEBISP")
+cat("There are", nrow(table.list), "tables in the DB2INST1 schema.\n")
+
+table.name <- "ODEBISP.TODEAYR6"
+head(table.name)
+table.name
+
+my_con <- odbcConnect("PROD", uid = "odemtpe", pwd = "bucak095")
+my_query <- "SELECT * FROM ODEBISP.TODEAYR6"
+my_result <- sqlQuery(my_con, my_query, errors= TRUE)
+View(my_result)
